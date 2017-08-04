580730b8301aee43d69c0c96bfb00c67e02e8f74
sarunjuntui77 <sarunjuntui@gmail.com>
2017-08-04 15:13:36 +0700

    merge

--- server.js
+++ server.js
@@ -30,16 +30,16 @@ connection.connect((err)=> {
   }
 });
 app.listen(1711,(a)=>{
-    console.log("App listening on port 1711");
- 
- });
+  console.log("App listening on port 1711");
+  
+});
 let dirName = __dirname;
 
 function cors(res){
-res.header("Content-Type", "application/json; charset=utf-8");
-res.header("Access-Control-Allow-Origin", "*");
-res.header("Access-Control-Allow-Methods", "POST, GET,OPTIONS");
-res.header('Access-Control-Allow-Headers', 'Accept, Authorization, Content-Type, X-Requested-With, Range');
+  res.header("Content-Type", "application/json; charset=utf-8");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "POST, GET,OPTIONS");
+  res.header('Access-Control-Allow-Headers', 'Accept, Authorization, Content-Type, X-Requested-With, Range');
 
 }
 
